68b89acc2cd243e0a7f2c04a843f37afc3ccb59e
tabain <tabain>
2015-11-09 15:26:44 +0500

    get / render view

--- server/api.js
+++ server/api.js
@@ -1,9 +1,9 @@
 var api = require('express')();
-var newGuests = require('./controllers/newGuests');
-var locations = require('./controllers/locations');
-
-api.get('/newGuests', newGuests.getGuest);
-api.post('/newGuests', newGuests.createGuest);
-api.get('/locations', locations.getLocations);
-
+//var newGuests = require('./controllers/newGuests');
+//var locations = require('./controllers/locations');
+//
+//api.get('/newGuests', newGuests.getGuest);
+//api.post('/newGuests', newGuests.createGuest);
+//api.get('/locations', locations.getLocations);
+//
 module.exports = api;
--- server/website.js
+++ server/website.js
@@ -4,12 +4,7 @@ var config = require('../config/config');
 var session = require('express-session');
 var logger = require('../config/logger');
 var db = require('../config/db');
-var guests = require('./controllers/guests');
-var locations = require('./controllers/locations');
-var newGuests = require('./controllers/newGuests');
 var admins = require('./controllers/admins');
-var frontdesk = require('./controllers/frontdesk');
-var newFrontdesk = require('./controllers/newFrontdesk');
 var base = require('./controllers/base');
 var MongoStore = require('connect-mongo')(session);
 var passport = require('passport');
@@ -34,10 +29,7 @@ website.use(session(sess));
 website.use(passport.initialize());
 website.use(passport.session());
 
-website.post('/guests', [base.isSessionAvailable], guests.createGuest);
-website.get('/locations', [base.isSessionAvailable], locations.getLocations);
-website.post('/newGuests', [base.isSessionAvailable], newGuests.createGuest);
-website.post('/visits', [base.isSessionAvailable], newGuests.createVisit);
+
 website.post('/login', passport.authenticate('local'), admins.me);
 website.post('/logout', base.logout);
 
@@ -48,34 +40,17 @@ website.post('/users', [admins.isAdmin], admins.createUser);
 website.put('/users/:userId', [admins.isAdmin], admins.updateUser);
 website.delete('/users/:userId', [admins.isAdmin], admins.deleteUser);
 
-website.get('/admins/newGuests', [newFrontdesk.isFrontDesk], newFrontdesk.listGuests);
-website.put('/admins/newGuests/:visitId',[newFrontdesk.isFrontDesk],newFrontdesk.updateGuest);
-website.delete('/admins/newGuests/:visitId', [frontdesk.isFrontDesk], newFrontdesk.deleteGuest);
-website.get('/admins/guests', [frontdesk.isFrontDesk], frontdesk.listGuests);
-website.post('/admins/guests', [frontdesk.isFrontDesk], frontdesk.createGuest);
-website.put('/admins/guests/:guestId', [frontdesk.isFrontDesk], frontdesk.updateGuest);
-website.put('/returning/guests/:guestId', frontdesk.updateGuest);
-
-website.delete('/admins/guests/:guestId', [frontdesk.isFrontDesk], frontdesk.deleteGuest);
-
 
 // Chart Data
-website.get('/data/promotions', [admins.isAdmin], admins.usageData);
+//website.get('/data/promotions', [admins.isAdmin], admins.usageData);
 
-website.get('/reports', [frontdesk.isFrontDesk], admins.generateReport);
+//website.get('/reports', [frontdesk.isFrontDesk], admins.generateReport);
 
 website.get('/', function (req, res, next) {
     req.session.isGuest = true;
-    if (req.headers.host === 'localhost:3000' || req.headers.host === 'greystar-stage.elasticbeanstalk.com' || req.headers.host === 'greystar-prod.elasticbeanstalk.com'){
-        res.render('main', {});
-    }
-    else if(req.headers.host === 'localhost:5060' || req.headers.host === 'nido-stage.elasticbeanstalk.com' || req.headers.host === 'nido-prod.elasticbeanstalk.com'){
-        res.render('main-nido', {});
-
-    }
+    res.render('main', {});
 });
-website.get('/guests', guests.getGuest);
-website.get('/newGuests', newGuests.getGuest);
+
 
 website.get('*', function (req, res, next) {
     res.status(404).render('404', {});
